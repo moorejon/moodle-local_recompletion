9035fc89f6704f635adec111393db68d66bc35b5
David Saylor <david.saylor.jr@gmail.com>
2019-07-23 05:23:03 -0800

    HER-153 Fixed web service description and reapplied bug fix related to duration value

--- externallib.php
+++ externallib.php
@@ -238,20 +238,27 @@ class local_recompletion_external extends external_api {
         $config = $DB->get_records_menu('local_recompletion_config', array('course' => $params['courseid']), '', 'name, value');
         $idmap = $DB->get_records_menu('local_recompletion_config', array('course' => $params['courseid']), '', 'name, id');
 
+        $daybasedvariables = array('recompletionduration', 'notificationstart', 'frequency');
         foreach ($setnames as $name) {
             if (isset($params['settings'][$name])) {
                 $value = $params['settings'][$name];
             } else {
-                if ($name == 'recompletionemailsubject'
-                    || $name == 'recompletionemailbody'
-                    || $name == 'recompletionremindersubject'
-                    || $name == 'recompletionreminderbody') {
-                    $value = '';
-                } else {
-                    $value = 0;
-                }
+                $value = null;
             }
-            if (!isset($config[$name]) || $config[$name] <> $value) {
+            if ((!is_null($value) && $config[$name] <> $value) || !isset($config[$name])) {
+                if (in_array($name, $daybasedvariables)) {
+                    $value = $value * 86400;
+                }
+                if (is_null($value)) {
+                    if ($name == 'recompletionemailsubject'
+                            || $name == 'recompletionemailbody'
+                            || $name == 'recompletionremindersubject'
+                            || $name == 'recompletionreminderbody') {
+                        $value = '';
+                    } else {
+                        $value = 0;
+                    }
+                }
                 $rc = new stdclass();
                 if (isset($idmap[$name])) {
                     $rc->id = $idmap[$name];
@@ -263,10 +270,6 @@ class local_recompletion_external extends external_api {
                     $DB->insert_record('local_recompletion_config', $rc);
                 } else {
                     $DB->update_record('local_recompletion_config', $rc);
-                }
-                if ($name == 'enable' && empty($value)) {
-                    // Don't overwrite any other settings when recompletion disabled.
-                    break;
                 }
             }
         }
@@ -589,7 +592,7 @@ class local_recompletion_external extends external_api {
             $rs = $DB->get_recordset('local_recompletion_cc', array('course' => $params['courseid']));
         }
 
-        $return = array();
+        $return = array('completions' => array());
 
         foreach ($rs as $recompletion) {
             $return['completions'][] = (array) $recompletion;

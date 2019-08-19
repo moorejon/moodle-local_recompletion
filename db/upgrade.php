6cd7797baa0880517cff671ac7ad6b56a9519bba
David Saylor <kingdavid127@gmail.com>
2019-08-19 06:03:58 -0800

    HER-187 Bumped version to trigger upgrade due to duplicate version update from another issue

--- db/upgrade.php
+++ db/upgrade.php
@@ -384,11 +384,11 @@ function xmldb_local_recompletion_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2019062001, 'local', 'recompletion');
     }
 
-    if ($oldversion < 2019081400) {
+    if ($oldversion < 2019081500) {
         $table = new xmldb_table('local_recompletion_config');
         $key = new xmldb_key('mdl_locarecoconf_cona_ix', XMLDB_KEY_UNIQUE, array('course', 'name'));
         $dbman->add_key($table, $key);
-        upgrade_plugin_savepoint(true, 2019081400, 'local', 'recompletion');
+        upgrade_plugin_savepoint(true, 2019081500, 'local', 'recompletion');
     }
 
     return true;
